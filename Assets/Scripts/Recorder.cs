79d0bdd68425a384545e4f2b613cbd6f25414b74
ruikyon <meranazunx@gmail.com>
2020-06-29 14:39:38 +0900

    [wip] add record function

---
+++ Assets/Scripts/Recorder.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor.Media;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Collections;
+using System.IO;
+
+public class Recorder: MonoBehaviour
+{
+    private MediaEncoder mediaEncoder;
+    private NativeArray<float> audioBuffer;
+    [SerializeField] private RenderTexture recordTexture;
+
+    private void Update()
+    {
+    }
+
+    public void RecordMovie()
+    {
+        var videoAttr = new VideoTrackAttributes
+        {
+            frameRate = new MediaRational(50),
+            width = 320,
+            height = 200,
+            includeAlpha = false
+        };
+
+        var audioAttr = new AudioTrackAttributes
+        {
+            sampleRate = new MediaRational(48000),
+            channelCount = 2,
+            language = "jp"
+        };
+
+        int sampleFramesPerVideoFrame = audioAttr.channelCount *
+            audioAttr.sampleRate.numerator / videoAttr.frameRate.numerator;
+
+        var encodedFilePath = Path.Combine(Path.GetTempPath(), "my_movie.mp4");
+
+        Texture2D tex = new Texture2D((int)videoAttr.width, (int)videoAttr.height, TextureFormat.RGBA32, false);
+
+        mediaEncoder = new MediaEncoder(encodedFilePath, videoAttr, audioAttr);
+        audioBuffer = new NativeArray<float>(sampleFramesPerVideoFrame, Allocator.Temp);
+    }
+
+    public async void AddData() 
+    {
+        var request = AsyncGPUReadback.Request(recordTexture);
+        while (!request.done) 
+        {
+            
+        }
+        //mediaEncoder.AddFrame();
+        //mediaEncoder.AddSamples(audioBuffer);
+    }
+}
