533c9fe145045731209f167c6d4157253d6386b2
ruikyon <meranazunx@gmail.com>
2020-08-16 00:30:24 +0900

    カメラ制御用スクリプト用意

---
+++ Assets/Scripts/VRGameCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGameCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
