fb5b0a0e8e28a0de73dbdc58a4a02ef6381d909d
ruikyon <meranazunx@gmail.com>
2020-09-09 23:13:51 +0900

    prepare demo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private Transform head;
+    private float heightOffset;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetHeightOffset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(x, 0, y);
+        
+        if (heightOffset < head.position.y)
+        {
+            var tmp = transform.position;
+            tmp.y = head.position.y - heightOffset;
+            transform.position = tmp;
+        } 
+    }
+
+    private void SetHeightOffset ()
+    {
+        heightOffset = head.position.y;
+    }
+}
--- Assets/Scripts/VRGameCamera.cs
+++ Assets/Scripts/VRGameCamera.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class VRGameCamera : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    [SerializeField] float maxSpeed;
+    public enum CameraPosition 
+    {
+        Back,
+        Front,
+        Hand,
+    }
+
+    [SerializeField] private Vector3 backPosition;
+    [SerializeField] private Vector3 frontPosition;
+
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject cameraUI;
+    [SerializeField] private float maxSpeed;
+    private Camera cameraEntity;
+    private CameraPosition state;
+    private bool isShow;
+
+    private void Awake()
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.GetComponent<Camera>() != null) 
+            {
+                cameraEntity = child.GetComponent<Camera>();
+                break;
+            }
+        }
+
+        isShow = false;
+        transform.position = target.position + Vector3.up;
+        SetCameraPosition(CameraPosition.Back);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +46,46 @@ public class VRGameCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            isShow = !isShow;
+            cameraUI.SetActive(isShow);
+            cameraEntity.gameObject.SetActive(isShow);
+        }
+
+        // TODO
+        //  以下は仮で、高速に移動した場合のことも検討した方がよさそう
+        //  あと高さは調整したさそうな気がするので、offset足すとかする
+        //transform.position = new Vector3(target.position.x, 1, target.position.z);
+        var to = target.position;
+        to.y = 1;
+        var from = transform.position;
+        from.y = 1;
+        transform.position = Vector3.Lerp(from, to, Time.deltaTime);
+    }
+
+    public void NextState() 
+    {
+        SetCameraPosition((CameraPosition)(((int)state + 1) % 3));
+    }
+
+    public void SetCameraPosition(CameraPosition position) 
+    {
+        switch(position)
+        {
+            case CameraPosition.Back:
+                cameraEntity.transform.localPosition = backPosition;
+                cameraEntity.transform.LookAt(transform);
+                break;
+            case CameraPosition.Front:
+                cameraEntity.transform.localPosition = frontPosition;
+                cameraEntity.transform.LookAt(transform);
+                break;
+            case CameraPosition.Hand:
+                Debug.Log("hand");
+                break;        
+        }
+
+        state = position;
     }
 }
