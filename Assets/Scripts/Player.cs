7a0a1eff7662c14e76618babe4b530da86233197
ruikyon <meranazunx@gmail.com>
2020-09-10 13:15:44 +0900

    IKの追加やUI調整など

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private Transform head;
-    private float heightOffset;
+    private Transform rig;
+    [SerializeField] private float heightOffset;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rig = head.parent;
     }
 
     // Start is called before the first frame update
@@ -22,21 +24,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SetHeightOffset();
+        }
+
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector3(x, 0, y);
-        
+
+        var pos = head.position;
+        pos.y = 0;
+
         if (heightOffset < head.position.y)
         {
-            var tmp = transform.position;
-            tmp.y = head.position.y - heightOffset;
-            transform.position = tmp;
-        } 
+            pos.y = head.position.y - heightOffset;
+        }
+        transform.position = pos;
     }
 
     private void SetHeightOffset ()
     {
-        heightOffset = head.position.y;
+        var sub = heightOffset - head.position.y;
+        var rigpos = rig.position;
+        rigpos.y += sub;
+        rig.position = rigpos;
     }
 }
